db6f81b51f90aadd3cfaea29fa765a6e27fe87e1
GiantTree <webmaster@gianttree.de>
2017-02-11 23:47:26 +0100

    Move README.java into a proper package.

--- src/main/java/org/gtlp/ui/README.java
+++ src/main/java/org/gtlp/ui/README.java
@@ -1,6 +1,8 @@
+package org.gtlp.ui;
+
 /**
  * JavaDoc is unavailable due to the fact that this project is written in Kotlin.
- * <p>
+ *
  * For documentation, please refer to the sources.
  */
 public class README {
