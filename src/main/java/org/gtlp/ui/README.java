a71b2508d49176b646795cf6f537211c806e6c8b
GiantTree <webmaster@gianttree.de>
2017-04-15 21:44:43 +0200

    Use Dokka for documentation. Update libraries while we are at it.

--- src/main/java/org/gtlp/ui/README.java
+++
@@ -1,9 +0,0 @@
-package org.gtlp.ui;
-
-/**
- * JavaDoc is unavailable due to the fact that this project is written in Kotlin.
- *
- * For documentation, please refer to the sources.
- */
-public class README {
-}
